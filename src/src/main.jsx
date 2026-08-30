25a80e82276522506c8a9698fef1dc71d465c567
RIRI1427 <47zm2p6p2w@privaterelay.appleid.com>
2026-08-30 15:44:06 +0900

    Delete src/src/main.jsx

--- src/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import DietTracker from '../DietTracker.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <DietTracker />
-  </React.StrictMode>,
-)
